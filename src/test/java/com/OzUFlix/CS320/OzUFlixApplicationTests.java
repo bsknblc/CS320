b36cc358ae01c18855bae54eed9e3e252261b5b5
Başak Balcı <bbalci@kandy.io>
2021-12-23 00:18:51 +0300

    deneme

--- src/test/java/com/OzUFlix/CS320/OzUFlixApplicationTests.java
+++ src/test/java/com/OzUFlix/CS320/OzUFlixApplicationTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class OzUFlixApplicationTests {
 
@@ -54,6 +56,7 @@ class OzUFlixApplicationTests {
 		movie.setName("Inception");
 		movie.setTopic(topic);
 		movie.setDirector(director);
+		assertTrue(movie.getName().equals("Inception"));
 
 		User manager = new User();
 		manager.setName("Aylin");
