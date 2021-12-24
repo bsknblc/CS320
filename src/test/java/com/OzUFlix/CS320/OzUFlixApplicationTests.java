d6f5316528a408bd056fc20ef994d07cf7c214df
Aylin Aydın <aylin.aydin@ozu.edu.tr>
2021-12-24 22:10:15 +0300

    Test implemented.

--- src/test/java/com/OzUFlix/CS320/OzUFlixApplicationTests.java
+++ src/test/java/com/OzUFlix/CS320/OzUFlixApplicationTests.java
@@ -1,5 +1,6 @@
 package com.OzUFlix.CS320;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import com.OzUFlix.CS320.Model.*;
 import com.OzUFlix.CS320.Service.*;
@@ -61,6 +62,8 @@ class OzUFlixApplicationTests {
 		movie.setTopic(topic);
 		movie.setDirector(director);
 
+		available.getMovies().add(movie);
+
 		assertTrue(movie!=null);
 		assertTrue(movie.getName()=="Inception");
 		assertTrue(movie.getTopic()==topic);
@@ -116,16 +119,52 @@ class OzUFlixApplicationTests {
 		assertTrue(penalty.getRent()==rent);
 		assertTrue(penalty.getReturn_movie()==returnM);
 
+
+
+
 		availableService.save(available);
+		assertTrue(availableService.findAll()!=null);
 		availableService.save(notAvailable);
+		assertTrue(availableService.findAll()!=null);
 		directorService.save(director);
+		assertTrue(directorService.findAll()!=null);
 		topicService.save(topic);
+		assertTrue(topicService.findAll()!=null);
 		movieService.save(movie);
+		assertTrue(movieService.findAll()!=null);
 		userService.save(manager);
+		assertTrue(userService.findAll()!=null);
+
 		userService.save(user);
+		assertTrue(userService.findAll()!=null);
+
 		rentService.save(rent);
+		assertTrue(rentService.findAll()!=null);
+
 		return_movieService.save(returnM);
+		assertTrue(return_movieService.findAll()!=null);
+
 		penaltyService.save(penalty);
+		assertTrue(penaltyService.findAll()!=null);
+
+
+
+
+		/*assertEquals(directorService.findById(1).getId(),director.getId());
+		assertEquals(availableService.findById(1).getMovies(),available.getMovies());
+		assertTrue(availableService.findById(1).getId()==available.getId());
+		assertTrue(availableService.findById(1).getInfo()==available.getInfo());
+		assertTrue(availableService.findById(2).equals(notAvailable));
+		assertTrue(topicService.findById(1).equals(topic));
+		assertTrue(movieService.findById(1).equals(movie));
+		assertTrue(userService.findById(1).equals(manager));
+		assertTrue(userService.findById(2).equals(user));
+		assertTrue(rentService.findById(1).equals(rent));
+		assertTrue(return_movieService.findById(1).equals(returnM));
+		assertTrue(penaltyService.findById(1).equals(penalty));*/
+
+
+
 
 	}
 
