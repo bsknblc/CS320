f43ceda41ea7ea40dcc442470ac3a255eb3be47c
Ekin Devrim Şahinkaya <devrim.sahinkaya@ozu.edu.tr>
2021-12-25 00:19:01 +0300

    Frontend

---
+++ src/main/resources/Static/login-page.js
@@ -0,0 +1,20 @@
+import UserService from java/com.OzUFlix.CS320/Service/UserService;
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (validateUser(username, password)) {
+        console.log("ok");
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        console.log("no");
+        loginErrorMsg.style.opacity = 1;
+    }
+})
